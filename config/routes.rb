c7584e0f4f5f0076f8a7aec7230da73a69fb4eb2
Luis Miño B <l.mino.bustos@gmail.com>
2023-08-01 18:43:46 -0400

    feat: index and styles created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/index', to: "pages#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
 end
